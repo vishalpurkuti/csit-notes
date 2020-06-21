75d2875ea07d6a36a1e21942e3597d6633d1a79a
vishalpurkuti <44137967+vishalpurkuti@users.noreply.github.com>
2020-06-21 22:23:15 +0545

    Create covid.js

---
+++ covid.js
@@ -0,0 +1,11 @@
+ <script>
+        function close_pop() {
+            document.getElementById('popup').style.visibility = 'hidden';
+            document.getElementById('overlay').style.visibility = 'hidden';
+        }
+        function open_pop() {
+            document.getElementById('popup').style.visibility = 'visible';
+
+        }
+
+    </script>
